0465f6230ce7753157922933b40c39f5b0857c29
Alex V <alex@avlaskin.com>
2025-01-20 11:35:58 -0600

    Add scripts for LLAMA 3B weights

---
+++ scripts/run_gsm8k_gen_4gpu.sh
@@ -0,0 +1,11 @@
+export OMP_NUM_THREADS=16
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+python run_src/do_generate.py \
+       --model_ckpt meta-llama/Llama-3.2-3B-Instruct
+       --dataset_name GSM8K \
+       --test_json_filename test_all \
+       --note tensor_parallelism \
+       --num_rollouts 16 \
+       --api vllm \
+       --model_parallel \
+       --tensor_parallel_size 4
--- scripts/run_gsm8k_generator.sh
+++ scripts/run_gsm8k_generator.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python run_src/do_generate.py \
+python run_src/do_generate.py \
     --dataset_name GSM8K \
     --test_json_filename test_all \
-    --model_ckpt mistralai/Mistral-7B-v0.1 \
+    --model_ckpt meta-llama/Llama-3.2-3B-Instruct
     --note default \
     --num_rollouts 16
