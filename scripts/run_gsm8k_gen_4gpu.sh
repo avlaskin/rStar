74bd5bc12f62114a44d25ec9b5bcd7673d2acd91
Alex V <alex@avlaskin.com>
2025-01-20 11:45:54 -0600

    Update scripts for reproducibility

--- scripts/run_gsm8k_gen_4gpu.sh
+++ scripts/run_gsm8k_gen_4gpu.sh
@@ -1,10 +1,13 @@
 export OMP_NUM_THREADS=16
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 python run_src/do_generate.py \
-       --model_ckpt meta-llama/Llama-3.2-3B-Instruct
+       --model_ckpt meta-llama/Llama-3.2-3B-Instruct \
        --dataset_name GSM8K \
        --test_json_filename test_all \
        --note tensor_parallelism \
+       --num_subquestions 5 \
+       --num_a1_steps 5 \
+       --disable_a5 \
        --num_rollouts 16 \
        --api vllm \
        --model_parallel \
--- scripts/run_gsm8k_generator.sh
+++ scripts/run_gsm8k_generator.sh
@@ -1,6 +1,9 @@
 python run_src/do_generate.py \
     --dataset_name GSM8K \
     --test_json_filename test_all \
-    --model_ckpt meta-llama/Llama-3.2-3B-Instruct
+    --num_subquestions 5 \
+    --num_a1_steps 5 \
+    --disable_a5 \
+    --model_ckpt meta-llama/Llama-3.2-3B-Instruct \
     --note default \
     --num_rollouts 16
